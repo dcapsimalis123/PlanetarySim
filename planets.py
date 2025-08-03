2e8872dcc05ee2028fb5618d1a8a92e643be89e2
Donovan Capsimalis <donovan.capsimalis@gmail.com>
2025-08-03 13:48:50 -0700

    incomplete implimentation of RK4, untested

--- planets.py
+++ planets.py
@@ -21,12 +21,37 @@ class Planet:
 
     # changes the velocity of the objects during the basic steps
     def step_vel(self,objList):
-        self.vel[0] = self.vel[0]+gvars.g*gvars.dt
+        # returns list of velocity changes and calculations from momentum changing effects
+        # zero index will be gravity/universal changes
+        # index beyond that will be interactions with other objects
+        # first index will be dist calc
+        # second through fourth index (expand as needed) will be unit vectors values
+        # third index will be gravity
+        velDeltas = np.array(len(objList),3) 
+        k = 0
+        velDeltas[k,0] = self.vel[0]+gvars.g*gvars.dt
         for obj in objList:
             if self.name != obj.name:
-                dist       = gvars.dist_calc(  self.pos, obj.pos)
-                unitVector = gvars.unit_vector(self.pos, obj.pos)
-                self.vel -= gvars.G*self.mass*obj.mass/(dist**2)*gvars.dt*unitVector
+                k += 1
+                velDeltas[k,0]   = gvars.dist_calc(  self.pos, obj.pos)
+                velDeltas[k,1:2] = gvars.unit_vector(self.pos, obj.pos)
+                velDeltas[k,3]   = obj.mass
+                # -= gvars.G*self.mass*obj.mass/(dist**2)*gvars.dt*unitVector
+        return velDeltas
+
+    # iterates the position and velocity values for the sim
+    def RK4_Method(self,obj):
+        for i in range(len(self.pos)):
+            temp = self.step_vel()
+            dist = temp[1:,0]
+            unitVector = temp[1:,1:]
+            k = np.zeros((2,4))
+            k[0,:] = self.pos[i] + self.vel[i]*gvars.dt
+            k[0,:] = sum([-1*gvars.G*self.mass*obj[i].mass/(dist[i]**2)*gvars.dt*unitVector[i]])
+            k[1,:] = (self.pos[i] + self.vel[i]*gvars.dt*0.5) + k[0,:]*gvars.dt*0.5
+            k[2,:]= (self.pos[i] + self.vel[i]*gvars.dt*0.5) + k[1,:]*gvars.dt*0.5
+            k[3,:] = (self.pos[i] + self.vel[i]*gvars.dt*0.5) + k[2,:]*gvars.dt*0.5
+            self.pos[i] += gvars.dt*(k[0,:]+2*k[1,:]+2*k[2,:]+k[3,:])/6 
 
     # returns the kinetic energy of the object
     def return_ke(self):
